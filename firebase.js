08eb6c3f086eb153cfb09a782da6a408421eec8f
J4mes-exe <jamesdishena@gmail.com>
2025-02-10 19:41:37 +0200

    Updated html and firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBhnXfxrd5Ktk-GuUcwx13ZMY6bhBao1Uw",
   authDomain: "next-starter-28a0f.firebaseapp.com",
   projectId: "next-starter-28a0f",
-  storageBucket: "next-starter-28a0f.firebasestorage.app",
+  storageBucket: "next-starter-28a0f.appspot.com",
   messagingSenderId: "899672520926",
   appId: "1:899672520926:web:c2ad8aa651ab7592dd2a4f",
   measurementId: "G-QMKXHKWS1G"
@@ -19,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
